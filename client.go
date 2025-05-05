1a664baecb09b1069b819aae337a2c6fd5b53183
Joey Akitoye <joseph.akitoye@piverworld.com>
2025-05-05 13:23:23 +0000

    feat(config): add configuration
    - add debug flag
    - add client naming flag
    - add max connections flag
    - add max reconnect wait flag
    
    Signed-off-by: Joey Akitoye <joseph.akitoye@piverworld.com>

--- client.go
+++ client.go
@@ -2,6 +2,9 @@ package nexor
 
 import (
 	"github.com/nats-io/nats.go"
+	"log"
+	"os"
+	"strconv"
 	"time"
 )
 
@@ -10,26 +13,81 @@ type Nexor struct {
 	js   nats.JetStreamContext
 }
 
+type nexorConfig struct {
+	ClientName string
+	Debug      bool
+	MaxConn    int
+	MaxRecon   int
+	ReconWait  int
+}
+
+func getConfig() *nexorConfig {
+	var debugMode = false
+	var clientName = "Nexor"
+	var maxConn, maxWait = 5, 5
+	debugModeValue, found := os.LookupEnv("NEXOR.DEBUG")
+	if found {
+		debugMode = debugModeValue == "true"
+	}
+
+	clientNameValue, found := os.LookupEnv("NEXOR.CLIENT")
+	if found {
+		clientName = clientNameValue
+	}
+
+	maxConnValue := os.Getenv("NEXOR.MAX_CONNECTIONS")
+	if maxConnValue != "" {
+		maxConn, _ = strconv.Atoi(maxConnValue)
+	}
+
+	maxWaitValue := os.Getenv("NEXOR.MAX_RECONNECT_WAIT")
+	if maxWaitValue != "" {
+		maxWait, _ = strconv.Atoi(maxWaitValue)
+	}
+
+	return &nexorConfig{
+		ClientName: clientName,
+		Debug:      debugMode,
+		MaxConn:    maxConn,
+		ReconWait:  maxWait,
+	}
+}
+
 // New initializes a new Nexor client with optional NATS options.
 // Usage: nexor.New("nats://localhost:4222")
 func New(url string, opts ...nats.Option) (*Nexor, error) {
+	cfg := getConfig()
 	if len(opts) == 0 {
 		opts = []nats.Option{
-			nats.Name("NexorClient"),
-			nats.MaxReconnects(5),
-			nats.ReconnectWait(2 * time.Second),
+			nats.Name(cfg.ClientName),
+			nats.MaxReconnects(cfg.MaxRecon),
+			nats.ReconnectWait(time.Duration(cfg.ReconWait) * time.Second),
 		}
 	}
 
 	conn, err := nats.Connect(url, opts...)
 	if err != nil {
+		if cfg.Debug {
+			log.Printf("🔌 Failed to connect to NATS: %v 🔌\n\n", err)
+			os.Exit(1)
+		}
+
 		return nil, err
 	}
 
 	js, err := conn.JetStream()
 	if err != nil {
+		if cfg.Debug {
+			log.Printf("🔌 Failed to connect to Jetstream: %v 🔌", err)
+			os.Exit(1)
+		}
+
 		conn.Close()
 		return nil, err
+	}
+
+	if cfg.Debug {
+		log.Printf("🚀 Connected to NATS server successful 🚀\n")
 	}
 
 	return &Nexor{conn: conn, js: js}, nil
